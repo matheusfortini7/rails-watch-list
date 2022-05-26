038efc634110f2ce194a69c0edc5f0baf4e9378c
Matheus Fortini <matheusfortini7@gmail.com>
2022-05-26 18:13:50 -0300

    esse commit cria 90% do MVC do sistema de filmes

---
+++ app/helpers/bookmasrks_helper.rb
@@ -0,0 +1,2 @@
+module BookmasrksHelper
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+10.times do
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Lorem.sentence,
+    poster_url: Faker::Internet.url,
+    rating: rand(0..10)
+  )
+end
